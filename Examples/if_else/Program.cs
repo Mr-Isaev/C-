7e755849da2c1cc7d4cee633afb6a64b819c137e
«Alexandr» <>
2022-12-14 17:27:07 +0500

    Добавили проект с условиями

---
+++ Examples/if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Маша"){
+	Console.WriteLine("Привет, Мой господин");
+}
+else{
+	Console.Write("Привет, ");
+	Console.Write(username);
+}
